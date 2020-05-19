05d64ded0c4827bb9b723fff71153ddb8dbab8c0
sagarkrsd <sagar.kumar@mayadata.io>
2020-05-19 20:05:31 +0530

    use nameselectors and multiple matchReferenceExpressions
    Signed-off-by: sagarkrsd <sagar.kumar@mayadata.io>

--- types/annotations.go
+++ types/annotations.go
@@ -3,7 +3,7 @@ package types
 const (
 	// AnnotationPrefix is the prefix used across all
 	// the annotations supported in this project
-	AnnotationPrefix string = "dao.mayadata.io"
-	// AnnKeyOpenEBSUID is the annotation that refers to OpenEBS UID
+	AnnotationPrefix string = "openebs-upgrade.dao.mayadata.io"
+	// AnnKeyOpenEBSUID is the annotation that refers to OpenEBS UID of openebs-upgrade
 	AnnKeyOpenEBSUID string = AnnotationPrefix + "/openebs-uid"
 )
